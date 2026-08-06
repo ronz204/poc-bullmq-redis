d6ae1de04427454279a9e2c0d97a40473f0c8710
roney ruiz <roneyruizrojas@gmail.com>
2026-08-05 21:57:58 -0600

    chore(app): drop complete old project files and structure

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import App from "./App.vue";
-import { createApp } from "vue";
-import "@assets/styles/main.css";
-
-const app = createApp(App);
-app.mount("#app");
--- src/zix/types/Marker.ts
+++
@@ -1,9 +0,0 @@
-import type { ZixPoint } from "./Geometric";
-
-export interface ZixMarker<ZixData> {
-  id: number;
-  label: string;
-  floor: number;
-  data: ZixData;
-  position: ZixPoint;
-};
